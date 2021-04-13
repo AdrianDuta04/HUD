a70027548a24ddc9d502c70a9c2f31354feac389
Adrian <adrianduta98@live.com>
2021-04-13 14:15:01 +0300

    solved line detection problems

--- src/laneRecognition/laneRecog.py
+++ src/laneRecognition/laneRecog.py
@@ -1,5 +1,7 @@
 import os
 import re
+import time
+
 import cv2
 import numpy as np
 
@@ -9,26 +11,34 @@ theta = np.pi / 180
 
 
 def interested_region(img):
-    imshape = img.shape
-    lower_left = [imshape[1] / 9, imshape[0]]
-    lower_right = [imshape[1] - imshape[1] / 9, imshape[0]]
-    top_left = [imshape[1] / 2 - imshape[1] / 8, imshape[0] / 2 + imshape[0] / 10]
-    top_right = [imshape[1] / 2 + imshape[1] / 8, imshape[0] / 2 + imshape[0] / 10]
-    polygons = np.array([lower_left, top_left, top_right, lower_right])
-    if len(img.shape) > 2:
-        mask_color_ignore = 255 * img.shape[2]
-    else:
-        mask_color_ignore = 255
-    cv2.fillPoly(np.zeros_like(img), polygons, mask_color_ignore)
-    return cv2.bitwise_and(img, np.zeros_like(img))
+    height = img.shape[0]
+    polygons = np.array([
+        [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(img)
 
+    # Fill poly-function deals with multiple polygon
+    cv2.fillPoly(mask, polygons, 255)
 
+    # Bitwise operation between canny image and mask image
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
 
 
 def draw_lines(img, lines, color=(255, 0, 0), thickness=7):
     for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+        if len(line) == 4:
+            x1, y1, x2, y2 = line
+            if abs(x1 - x2) > 800:
+                x2 = int(x1 + (x2 - x1) / 2)
+                coord =[[x1,y1],[x1/1.2,y1/1.1],[int((x1+x2)/2.8),int((y1+y2)/2.5)],[x2/1.1,y2],[int((x1+x2)/2.6),int((y1+y2)/2.8)]]
+                print(coord)
+                coord=np.array(coord,np.int32)
+                coord=coord.reshape((-1,1,2))
+                isClosed=False
+                img=cv2.polylines(img,[coord],isClosed,color,thickness)
+            else:
+                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
     return img
 
 
@@ -51,42 +61,57 @@ def create_coordinates(image, line_parameters):
 
 
 def average_slope_intercept(image, lines):
+    global left_line, right_line
     left_fit = []
     right_fit = []
+    left_line = []
+
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
-
-        # It will fit the polynomial and the intercept and slope
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
         intercept = parameters[1]
         if slope < 0:
             left_fit.append((slope, intercept))
+            left_fit_average = np.average(left_fit, axis=0)
+            left_line = create_coordinates(image, left_fit_average)
         else:
             right_fit.append((slope, intercept))
-
-    left_fit_average = np.average(left_fit, axis=0)
-    right_fit_average = np.average(right_fit, axis=0)
-    left_line = create_coordinates(image, left_fit_average)
-    right_line = create_coordinates(image, right_fit_average)
+            right_fit_average = np.average(right_fit, axis=0)
+            right_line = create_coordinates(image, right_fit_average)
+    if len(left_line) == 0 :
+        left_line[0] = right_line[0] + 300
+        left_line[1] = right_line[1]
+        left_line[2] = right_line[2] + 300
+        left_line[3] = right_line[3]
+        print("completer")
     return np.array([left_line, right_line])
 
 
-cap = cv2.VideoCapture("datasets\test2.mp4")
+cap = cv2.VideoCapture("test_video/challenge.mp4")
+i = 0
 while cap.isOpened():
-    _, frame = cap.read()
-    canny_image = canny_edge_detector(frame)
-    cropped_image = interested_region(canny_image)
+    try:
+        _, frame = cap.read()
+        if frame is None:
+            print("end of video file")
+            break
+        combo_image = frame
+        if i % 24 == 0:
+            canny_image = canny_edge_detector(frame)
+            cropped_image = -interested_region(canny_image)
+            lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100,
+                                    np.array([]), minLineLength=40,
+                                    maxLineGap=5)
 
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100,
-                            np.array([]), minLineLength=40,
-                            maxLineGap=5)
-
-    averaged_lines = average_slope_intercept(frame, lines)
-    line_image = draw_lines(frame, averaged_lines)
-    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    cv2.imshow("results", combo_image)
-
+            averaged_lines = average_slope_intercept(frame, lines)
+        line_image = draw_lines(frame, averaged_lines)
+        combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+        cv2.imshow("results", combo_image)
+        i += 1
+        time.sleep(1)
+    except:
+        print("Image quality does not allow line recognition")
     # When the below two will be true and will press the 'q' on
     # our keyboard, we will break out from the loop
 
