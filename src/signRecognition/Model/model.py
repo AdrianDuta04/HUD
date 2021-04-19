5b4a74f431899de1922ec4c5790feb200a8c29a2
Adrian <adrianduta98@live.com>
2021-04-19 20:35:40 +0300

    code clean-up

--- src/signRecognition/Model/model.py
+++ src/signRecognition/Model/model.py
@@ -1,31 +1,29 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D , MaxPooling2D, Dense , Flatten , Dropout , BatchNormalization
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
 from time import time
 
 height = 32
 width = 32
 depth = 3
 model = Sequential()
-shape =(height , width ,depth)
+shape = (height, width, depth)
 
-model.add(Conv2D(16, (5, 5), padding="same",activation="relu",input_shape=shape))
+model.add(Conv2D(16, (5, 5), padding="same", activation="relu", input_shape=shape))
 model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-
-model.add(Conv2D(32, (3, 3),padding="same", activation="relu"))
+model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
 model.add(BatchNormalization(axis=-1))
-model.add(Conv2D(32, (3, 3), padding="same",activation="relu"))
+model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
 model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
-
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
 model.add(Dropout(rate=0.25))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
 model.add(BatchNormalization(axis=-1))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
 model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(rate=0.25))
 
 model.add(Flatten())
@@ -39,7 +37,6 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 
 now = time()
 model.save("../compiled_models/sign_recognition_model_last.h5")
-model.save("../compiled_models/old_models/sign_recognition_model"+str(int(now))+".h5")
+model.save("../compiled_models/old_models/sign_recognition_model" + str(int(now)) + ".h5")
 
 model.summary()
-
