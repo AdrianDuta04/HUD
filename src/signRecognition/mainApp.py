8feb25d5541df479e0ab4ad9116b41bf342a95b1
Adrian <adrianduta98@live.com>
2021-05-11 15:52:57 +0300

    Improved GUI with preferences added

--- src/signRecognition/mainApp.py
+++ src/signRecognition/mainApp.py
@@ -1,7 +1,57 @@
-
 import sys
+import json
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QFileDialog, QLabel, QDialog, QGridLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QFileDialog, QLabel, QDialog, \
+    QGridLayout, QCheckBox
+
+
+class LaneMain(QMainWindow):
+    def __init__(self, parent=None):
+        super(LaneMain, self).__init__(parent)
+        self.left = 100
+        self.top = 100
+        self.width = 600
+        self.height = 400
+        self.dialogs = list()
+        self.imageFile = ""
+        self.setWindowTitle("Lane Recognition")
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        # Creare textBox
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 20)
+        self.textbox.resize(280, 30)
+
+        # Creare buton de incarcare fisier
+        self.button1 = self.createButton(300, 20, 100, 30, 'Incarcare fisier')
+        # adaugarea actiunii butonului => functia uploadFile
+        self.button1.clicked.connect(self.uploadFile)
+        self.label2 = QLabel(self)
+        self.label2.move(20, 90)
+
+    def createButton(self, y_move, x_move, y_size, x_size, text):
+        button = QPushButton(text, self)
+        button.move(y_move, x_move)
+        button.resize(y_size, x_size)
+        return button
+
+    def createLabel(self, y_move, x_move, text):
+        label = QLabel(self)
+        label.move(y_move, x_move)
+        label.setText(text)
+        label.adjustSize()
+        return label
+
+    def doNothig(self):
+        self.close()
+
+    def uploadFile(self):
+        self.textbox = QLineEdit(self)
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Open file')
+        name = file_name.split('/')
+        self.textbox.setText(file_name)
+        self.label2.setText(name[-1])
+        self.label2.adjustSize()
 
 
 class SignMain(QMainWindow):
@@ -12,7 +62,7 @@ class SignMain(QMainWindow):
         self.width = 600
         self.height = 400
         self.dialogs = list()
-        self.imageFile=""
+        self.imageFile = ""
         self.setWindowTitle("Sign Recognition")
         self.setGeometry(self.left, self.top, self.width, self.height)
         # Creare textBox
@@ -21,19 +71,18 @@ class SignMain(QMainWindow):
         self.textbox.resize(280, 30)
 
         # Creare buton de incarcare fisier
-        self.button1 = QPushButton('Incarcare fisier', self)
-        self.button1.move(300, 20)
-        self.button1.resize(100, 30)
+        self.button1 = self.createButton(300, 20, 100, 30, 'Incarcare fisier')
         # adaugarea actiunii butonului => functia uploadFile
         self.button1.clicked.connect(self.uploadFile)
 
         self.label2 = QLabel(self)
         self.label2.move(20, 90)
-
-        # self.label3 = QLabel(self)
-        # pixmap = QPixmap('image.jpeg')
-        # self.label3.setPixmap(pixmap)
-        # self.label3.move(100,100)
+        self.label3 = QLabel(self)
+        self.lb = QLabel(self)
+        self.lbPredict = QLabel(self)
+        self.predictBut = self.createButton(200, 120, 100, 30, "Predict")
+        self.predictBut.hide()
+        self.predictBut.clicked.connect(self.predict)
 
     def createButton(self, y_move, x_move, y_size, x_size, text):
         button = QPushButton(text, self)
@@ -41,7 +90,7 @@ class SignMain(QMainWindow):
         button.resize(y_size, x_size)
         return button
 
-    def createLabel(self,y_move,x_move,text):
+    def createLabel(self, y_move, x_move, text):
         label = QLabel(self)
         label.move(y_move, x_move)
         label.setText(text)
@@ -56,13 +105,28 @@ class SignMain(QMainWindow):
         self.dialogs.append(dialog)
         dialog.show()
 
+    def predict(self):
+        pixmap2 = QPixmap(self.imageFile)
+        self.lbPredict.resize(160, 160)
+        self.lbPredict.move(320, 120)
+        self.lbPredict.setPixmap(pixmap2.scaled(self.lbPredict.size(), Qt.IgnoreAspectRatio))
+        self.label3.move(370, 100)
+        self.label3.setText(self.imageFile)
+        self.label3.adjustSize()
+
     def uploadFile(self):
         self.textbox = QLineEdit(self)
         file_name, _ = QFileDialog.getOpenFileName(self, 'Open file')
-        name=file_name.split('/')
+        name = file_name.split('/')
         self.textbox.setText(file_name)
         self.label2.setText(name[-1])
         self.label2.adjustSize()
+        self.imageFile = file_name
+        pixmap = QPixmap(file_name)
+        self.lb.resize(160, 160)
+        self.lb.move(20, 120)
+        self.lb.setPixmap(pixmap.scaled(self.lb.size(), Qt.IgnoreAspectRatio))
+        self.predictBut.show()
 
 
 class UpdateDiagContinue(QMainWindow):
@@ -76,23 +140,26 @@ class UpdateDiagContinue(QMainWindow):
         self.setWindowTitle("Update")
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.text = self.createLabel(60, 20, 'Do you want to check for an update?\n It might take a few minutes')
+        self.box = QCheckBox("Do you want to save this option?", self)
+        self.box.move(200, 20)
+        self.box.resize(240, 300)
         self.button1 = self.createButton(130, 60, 100, 50, 'Yes')
         self.button1.clicked.connect(self.confirmUpdate)
         self.button2 = self.createButton(250, 60, 100, 50, 'No')
-        self.button2.clicked.connect(self.doNothig)
-        self.text2 = self.createLabel(60,20,"Please wait while we check")
+        self.button2.clicked.connect(self.doNothingAndSave)
+        self.text2 = self.createLabel(60, 20, "Please wait while we check")
         self.text2.hide()
-        self.text3 = self.createLabel(60,70,"An update is ready to be downloaded")
+        self.text3 = self.createLabel(60, 70, "An update is ready to be downloaded")
         self.text3.hide()
         self.text4 = self.createLabel(60, 70, "Download successful")
         self.text4.hide()
-        self.button3=self.createButton(130,130,100,50,"Yes")
+        self.button3 = self.createButton(130, 130, 100, 50, "Yes")
         self.button3.clicked.connect(self.confirmDownload)
         self.button3.hide()
         self.button4 = self.createButton(250, 130, 100, 50, "No")
         self.button4.clicked.connect(self.doNothig)
         self.button4.hide()
-        self.button5=self.createButton(200,130,100,50,'Ok')
+        self.button5 = self.createButton(200, 130, 100, 50, 'Ok')
         self.button5.clicked.connect(self.continueNormalExecution)
         self.button5.hide()
 
@@ -105,15 +172,24 @@ class UpdateDiagContinue(QMainWindow):
     def doNothig(self):
         self.close()
 
+    def doNothingAndSave(self):
+        data=[]
+        if self.box.isChecked():
+            with open("preference.json",'w') as output:
+                data.append({'checkUpdate':'no'})
+                json.dump(data, output)
+        self.close()
+
     def confirmUpdate(self):
         self.button1.hide()
         self.button2.hide()
         self.text.hide()
+        self.box.hide()
         self.text3.show()
         self.button3.show()
         self.button4.show()
 
-    def createLabel(self,y_move,x_move,text):
+    def createLabel(self, y_move, x_move, text):
         label = QLabel(self)
         label.move(y_move, x_move)
         label.setText(text)
@@ -149,19 +225,19 @@ class UpdateDiag(QMainWindow):
         self.button1.clicked.connect(self.confirmUpdate)
         self.button2 = self.createButton(250, 60, 100, 50, 'No')
         self.button2.clicked.connect(self.doNothig)
-        self.text2 = self.createLabel(60,20,"Please wait while we check")
+        self.text2 = self.createLabel(60, 20, "Please wait while we check")
         self.text2.hide()
-        self.text3 = self.createLabel(60,70,"An update is ready to be downloaded")
+        self.text3 = self.createLabel(60, 70, "An update is ready to be downloaded")
         self.text3.hide()
         self.text4 = self.createLabel(60, 70, "Download successful")
         self.text4.hide()
-        self.button3=self.createButton(130,130,100,50,"Yes")
+        self.button3 = self.createButton(130, 130, 100, 50, "Yes")
         self.button3.clicked.connect(self.confirmDownload)
         self.button3.hide()
         self.button4 = self.createButton(250, 130, 100, 50, "No")
         self.button4.clicked.connect(self.doNothig)
         self.button4.hide()
-        self.button5=self.createButton(200,130,100,50,'Ok')
+        self.button5 = self.createButton(200, 130, 100, 50, 'Ok')
         self.button5.clicked.connect(self.doNothig)
         self.button5.hide()
 
@@ -182,7 +258,7 @@ class UpdateDiag(QMainWindow):
         self.button3.show()
         self.button4.show()
 
-    def createLabel(self,y_move,x_move,text):
+    def createLabel(self, y_move, x_move, text):
         label = QLabel(self)
         label.move(y_move, x_move)
         label.setText(text)
@@ -252,7 +328,7 @@ class App(QMainWindow):
         self.button1.clicked.connect(self.signRecog)
 
         self.button2 = self.createButton(30, 80, 500, 50, 'Recunoasterea benzii de mers')
-        self.button2.clicked.connect(self.doNothig)
+        self.button2.clicked.connect(self.laneDialog)
 
         self.button3 = self.createButton(30, 300, 200, 50, 'Mod Admin')
         self.button3.clicked.connect(self.doNothig)
@@ -262,10 +338,12 @@ class App(QMainWindow):
 
         self.button5 = self.createButton(300, 300, 200, 50, 'Exit')
         self.button5.clicked.connect(self.closeDialog)
+        self.textbox = QLineEdit(self)
+        self.textbox.hide()
 
     def uploadFile(self):
-        self.textbox = QLineEdit(self)
-        file_name, _ = QFileDialog.getOpenFileName(self, 'Open file', 'C:\\', "Audio files (*.jpg)")
+        self.textbox.show()
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Open file')
         self.textbox.setText(file_name)
 
     def doNothig(self):
@@ -287,8 +365,20 @@ class App(QMainWindow):
         self.dialogs.append(dialog)
         dialog.show()
 
+    def laneDialog(self):
+        dialog = LaneMain(self)
+        self.dialogs.append(dialog)
+        dialog.show()
+
     def signRecog(self):
-        dialog = UpdateDiagContinue(self)
+        with open('preference.json') as json_file:
+            data = json.load(json_file)
+            print(data[0]['checkUpdate'])
+        if data[0]['checkUpdate'] == 'no':
+            print("sal")
+            dialog=SignMain(self)
+        else:
+            dialog = UpdateDiagContinue(self)
         self.dialogs.append(dialog)
         dialog.show()
 
